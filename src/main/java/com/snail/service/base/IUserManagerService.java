9a800b912bddc2aab554329e2ba02e309ef5e0f0
wangwu <mr_zhangsun@outlook.com>
2017-10-15 14:08:33 +0800

    用户详情查询

--- src/main/java/com/snail/service/base/IUserManagerService.java
+++ src/main/java/com/snail/service/base/IUserManagerService.java
@@ -1,9 +1,9 @@
 package com.snail.service.base;
 
-import com.snail.pojo.form.EmployeeForm;
-import com.snail.pojo.vo.EmployeeInfoVo;
+import com.snail.pojo.domain.Employee;
+import com.snail.pojo.form.EmployeeInsertForm;
+import com.snail.pojo.form.EmployeeQueryForm;
 
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -14,7 +14,23 @@ public interface IUserManagerService {
 
     /**
      * 查询用户列表
+     *
      * @return 用户列表
      */
-    Map<String, Object> listEmployees(EmployeeForm form);
+    Map<String, Object> listEmployees(EmployeeQueryForm form);
+
+    /**
+     * 根据用户的ID查询用户详情
+     *
+     * @param id 用户ID
+     * @return 用户详情对象
+     */
+    Employee getEmployeeById(Integer id);
+
+    /**
+     * 添加用户
+     *
+     * @param form 用户信息
+     */
+    void insertEmployee(EmployeeInsertForm form);
 }
